3ea052f65e23704be5df503bb837cd4d0b988a26
TedYeh <58154758+TedYeh@users.noreply.github.com>
2022-12-07 23:07:27 +0800

    add mail db

--- data/build_db.py
+++ data/build_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 from sqlite3 import Error
 
 def create_connection(db_name):
@@ -27,25 +28,51 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def main():
-    database = r"C:\sqlite\db\pythonsqlite.db"
+def insert_data(conn, table, data):
+    command = f"INSERT INTO {table} VALUES({''.join(['?,' for _ in range(len(data)-1)]+['?'])})"
+    #print(command)
+    try:
+        c = conn.cursor()
+        c.execute(command, tuple(data))
+        conn.commit()
+    except Error as e:
+        print(e)
 
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
+
+def read_csv(file_name):
+    with open(file_name, newline = '', encoding = 'utf-8') as csvfile:
+        n = 0
+        rows = csv.reader(csvfile)
+        for row in rows:
+            if n == 0:
+                n += 1
+                continue
+            yield row
+
+def main():
+    database = r".\db\messagesqlite.db"
+
+    sql_create_message_table = """ CREATE TABLE IF NOT EXISTS Messaging_1 (
+                                        contact_name text NOT NULL,
+                                        app_name text NOT NULL,
+                                        message text NOT NULL
                                     ); """
 
-    sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
-                                    id integer PRIMARY KEY,
-                                    name text NOT NULL,
-                                    priority integer,
-                                    status_id integer NOT NULL,
-                                    project_id integer NOT NULL,
-                                    begin_date text NOT NULL,
-                                    end_date text NOT NULL,
-                                    FOREIGN KEY (project_id) REFERENCES projects (id)
+    sql_create_calendar_table = """CREATE TABLE IF NOT EXISTS Calendar_1 (
+                                    name text NOT NULL,                                    
+                                    location text,  
+                                    content text, 
+                                    participant text,
+                                    date DATE NOT NULL,
+                                    time text NOT NULL
+                                );"""
+    
+    sql_create_mail_table = """CREATE TABLE IF NOT EXISTS Mail_1 (
+                                    subject text,                                    
+                                    content text,  
+                                    copy_recipient text, 
+                                    recipient text NOT NULL,
+                                    sender text NOT NULL
                                 );"""
 
     # create a database connection
@@ -53,13 +80,40 @@ def main():
 
     # create tables
     if conn is not None:
-        # create projects table
-        create_table(conn, sql_create_projects_table)
+        # create message table
+        create_table(conn, sql_create_message_table)
 
-        # create tasks table
-        create_table(conn, sql_create_tasks_table)
+        # create calendar table
+        create_table(conn, sql_create_calendar_table)
+
+        # create mail table
+        create_table(conn, sql_create_mail_table)
     else:
         print("Error! cannot create the database connection.")
+    
+    c = conn.cursor()
+    for row in c.execute('SELECT * FROM Messaging_1 '):
+        print(row)
+    for row in c.execute('SELECT * FROM Calendar_1 '):
+        print(row)
+    for row in c.execute('SELECT * FROM Mail_1 '):
+        print(row)
+    
+    return conn
+    """"""
+
 
 if __name__ == "__main__":
-    main()
+    conn = main()
+
+    for data in read_csv('message_entityies.csv'):
+        print(data)
+        insert_data(conn, 'Messaging_1', data)
+
+    for data in read_csv('events.csv'):
+        print(data)
+        insert_data(conn, 'Calendar_1', data)
+
+    for data in read_csv('mail_entityies.csv'):
+        print(data)
+        insert_data(conn, 'Mail_1', data)
