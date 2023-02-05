6b14649755203a1a5319f7898d812b59c2a55ce6
Ted Yeh <kmes50215@gmail.com>
2023-02-05 21:25:39 +0800

    add history viewer

--- goal_generation/need_labeled/label_site.py
+++ goal_generation/need_labeled/label_site.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests, json, random
 import os, time, re
+from copy import deepcopy
 from datetime import datetime
 from pprint import pprint
 from flask import Flask, flash, request, redirect, url_for, render_template, session
@@ -44,13 +45,17 @@ def get_dialogue(file_name):
 
 def rewrite_dialogue(template_file, uttrs, label_time):
     dialogs = json.loads(open(template_file, encoding='utf-8-sig').read())
+    coherence_score = deepcopy(uttrs[0])
     for dialog in dialogs:
-        for turn, par_uttr in zip(dialog["turns"], uttrs):
+        for turn, par_uttr in zip(dialog["turns"], uttrs[1:]):
             turn["utterance"] = par_uttr[1]
             for frame in turn["frames"]:
                 frame["slots"] = find_slot(par_uttr[1], frame["actions"])
     print(os.path.split(template_file)[1])
-    with open('./labeled_time.txt', "a+") as ltf: ltf.write(os.path.split(template_file)[1]+' '+str(label_time)+'\n')
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    elements = [os.path.split(template_file)[1], str(label_time), coherence_score[1], dt_string]
+    with open('./labeled_elements.txt', "a+") as ltf: ltf.write(','.join(elements)+'\n')
     with open(os.path.join('./labeled_dialog', os.path.split(template_file)[1]), 'w', encoding='utf-8') as f:
         json.dump(dialogs, f, ensure_ascii=False, indent=4)
 
@@ -60,10 +65,13 @@ def index():
     dialogue_file = random.choice(os.listdir(dialogue_path))
     if request.method == 'POST':
        if not os.path.isdir('./labeled_dialog'): os.mkdir('./labeled_dialog')
-       with open('./labeled_dialogue.txt', "r") as lf:
-        dialogue_file = [line.rstrip('\n') for line in lf.readlines()]
+       with open('./labeled_dialogue.txt', "r") as ldf:
+        dialogue_file = [line.rstrip('\n') for line in ldf.readlines()]
         label_time = datetime.now().timestamp() - session['start_time']
-       rewrite_dialogue(os.path.join(dialogue_path, dialogue_file[-1]), dict(request.form).items(), label_time)
+       rewrite_dialogue(os.path.join(dialogue_path, dialogue_file[-1]), list(dict(request.form).items()), label_time)
+       with open('./labeled_dialogue.txt', "w") as lf:
+           for d_file in os.listdir('./labeled_dialog'):
+            lf.write(d_file+'\n')    
        return redirect(url_for('index'))
        
     session['start_time'] = datetime.now().timestamp()
@@ -75,7 +83,30 @@ def index():
     items, d_id = get_dialogue(os.path.join(dialogue_path, dialogue_file))
     return render_template('label_system.html', items=items, d_id=d_id)
 
+@app.route('/history')
+def history():
+    items = []
+    for label_dialog in os.listdir('labeled_dialog'):
+        an_item = dict(d_id=label_dialog.replace('.json', ''))
+        items.append(an_item)
+    return render_template('history.html', items=items)
+
+@app.route('/<file_name>')
+def load_labeled_dialog(file_name):
+    dialogue_path = './labeled_dialog'
+    items, d_id = get_dialogue(os.path.join(dialogue_path, file_name+'.json'))
+    return render_template('rewrited_dialogue.html', items=items, d_id=d_id)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3171, debug=True)
     #get_dialogue('./new_json/915.json')
+    '''
+    total_turns = 0
+    for d_file in os.listdir('./new_json'):
+        dialogs = json.loads(open(os.path.join('./new_json', d_file), encoding='utf-8-sig').read())
+        for dialog in dialogs:    
+            total_turns += len(dialog["turns"])
+            
+    print(total_turns/len(os.listdir('./new_json')))
+    '''
     
